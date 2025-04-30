0188cdb480ff08bd88aeded16a497f29c953c5be
Yuriy Bevov <yuriybevov@gmail.com>
2025-04-30 17:36:01 +0300

    добавил модификаторы для 1-го типа главного блока

---
+++ src/pug/components/_ui/sliders/top-banner-slider/script.js
@@ -0,0 +1,35 @@
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const slider = document.querySelector(".top-banner-slider");
+
+if (slider) {
+	const btnNext = slider.querySelector(".swiper-button-next");
+	const btnPrev = slider.querySelector(".swiper-button-prev");
+	const pagination = slider.querySelector(".swiper-pagination");
+
+	console.log(
+		"test",
+		pagination && pagination.dataset.dynamic,
+		pagination.dataset.dynamic,
+	);
+
+	new Swiper(slider, {
+		modules: [Navigation, Pagination],
+		slidesPerView: 1,
+		spaceBetween: 20,
+
+		navigation: {
+			nextEl: btnNext ? btnNext : null,
+			prevEl: btnPrev ? btnPrev : null,
+		},
+
+		pagination: {
+			el: pagination ? pagination : null,
+			dynamicBullets: pagination && pagination.dataset.dynamic ? true : false,
+			clickable: true,
+		},
+	});
+}
